f8bd9329a6b60dd3a06333d96f19fe3355496ee6
Liaoxyu <liaoxyu2023-01@outlook.com>
2025-08-30 13:10:28 +0800

    Enjoy the scenery!

---
+++ passagesharer/static/global/bg_img_height_controller.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const bg_img_viewer = document.querySelector('.bg_img_viewer');
+    if (bg_img_viewer) {
+        bg_img_viewer.style.height = `${window.innerHeight}px`;
+    };
+});

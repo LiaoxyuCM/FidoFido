6f65781a15e1b9a1348602926ab54df5d7cabca7
Liaoxyu <liaoxyu2023-01@outlook.com>
2025-09-30 20:49:30 +0800

    [Enhance update] Update style.css, deleted useless scripts

--- passagesharer/static/global/js/bg_img_height_controller.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const bg_img_viewer = document.querySelector('.bg_img_viewer');
-    if (bg_img_viewer) {
-        bg_img_viewer.style.height = `${window.innerHeight}px`;
-    };
-});
--- passagesharer/static/global/js/search_input_field_enter_monitor.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('input.search_text_field').addEventListener('keydown', (event) => {
-        if (event.key === 'Enter') {
-            window.location.href = '/search/' + document.querySelector('input.search_text_field').value;
-        };
-    });
-});
